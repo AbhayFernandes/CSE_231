c7e50aa034a11f8306e54fd80ed123f94479455f
Abhay Fernandes <abhay.fernandes@protonmail.com>
2023-01-26 14:04:32 -0500

    initial commit

---
+++ project01/main.py
@@ -0,0 +1,35 @@
+########################################################
+# Computer Project #1
+
+# Algorithm:
+#     prompt user for floating point value representing distance
+#     input the float
+#     store within various variables the following conversions:
+#         - meters
+#         - feet
+#         - miles 
+#         - furlongs
+#         - time in minutes to walk
+#     output these conversions (rounded to 3 decimal points).
+########################################################
+
+import math
+
+ROD_TO_METERS = 5.0292
+ROD_TO_FEET = 16.5
+ROD_TO_MILES = 0.003125
+ROD_TO_FURLONGS = 0.025
+ROD_TO_MINUTES = 0.06048399291
+
+rods = input("Input rods: ")
+rod_float = float(rods)
+print("\nYou input", round(rod_float, 3), "rods.\n")
+# Output the appropriate conversions by multiplying
+# the input by the conversion factor.
+print("Conversions")
+print("Meters:", round(rod_float * ROD_TO_METERS, 3))
+print("Feet:", round(rod_float * ROD_TO_FEET, 3))
+print("Miles:", round(rod_float * ROD_TO_MILES, 3))
+print("Furlongs:", round(rod_float * ROD_TO_FURLONGS, 3))
+print("Minutes to walk", round(rod_float, 3), \
+    "rods:", round(rod_float * ROD_TO_MINUTES, 3))
---
+++ project02/main.py
@@ -0,0 +1,87 @@
+###########################################################
+#  Computer Project #2
+#
+#  Algorithm
+#    prompt user for all the variables required
+#        begin by doing basic computations \
+#        nescessary for all branches.
+#        branch off based on the code the user inputted.
+#            complete the computations required for only that branch
+#        output the same generic format with the calculated values
+#        loop back to prompting the user based on the input of the user
+#    display closing message should the user choose to exit
+###########################################################
+
+import math
+MILEAGE_COST = 0.25
+BUDGET_DAILY_COST = 40
+DAILY_COST = 60
+DAILY_MILEAGE_ALLOWANCE = 100
+WEEKLY_BASE_COST = 190
+WEEKLY_LOW_MILEAGE_ALLOWANCE = 900
+WEEKLY_LOW_MILEAGE_COST = 100
+WEEKLY_HIGH_MILEAGE_ALLOWANCE = 1500
+WEEKLY_HIGH_MILEAGE_COST = 200
+
+
+print("\nWelcome to Horizons car rentals. \
+\n\nAt the prompts, please enter the following: \
+\n\tCustomer's classification code (a character: BD, D, W) \
+\n\tNumber of days the vehicle was rented (int)\
+\n\tOdometer reading at the start of the rental period (int)\
+\n\tOdometer reading at the end of the rental period (int)")
+
+done = input("\nWould you like to continue (A/B)? ")
+while done != 'B':
+    code = input("\nCustomer code (BD, D, W): ")
+    if code == 'BD' or code == 'D' or code == 'W':
+        days = input("\nNumber of days: ")
+        odometer_start = input("\nOdometer reading at the start: ")
+        odometer_end = input("\nOdometer reading at the end:   ")
+        days = int(days)
+        num_miles = ((int(odometer_end) - int(odometer_start))/10)
+
+        # Handle if the odometer reading is less than the starting reading
+        if num_miles < 0:
+            num_miles += 100000
+
+        if code == 'BD':
+            cost = BUDGET_DAILY_COST * days + MILEAGE_COST * num_miles
+
+        elif code == 'D':
+            cost = DAILY_COST * days
+            if num_miles >= days * DAILY_MILEAGE_ALLOWANCE:
+                cost += MILEAGE_COST * \
+                (num_miles - (days * DAILY_MILEAGE_ALLOWANCE))
+
+        elif code == 'W':
+            weeks = days / 7
+            ceil_weeks = math.ceil(weeks)
+            cost = WEEKLY_BASE_COST * ceil_weeks
+            if num_miles < (WEEKLY_LOW_MILEAGE_ALLOWANCE * weeks):
+                pass
+
+            elif (WEEKLY_LOW_MILEAGE_ALLOWANCE * weeks) < num_miles and \
+            num_miles < (WEEKLY_HIGH_MILEAGE_ALLOWANCE * weeks):
+                cost += WEEKLY_LOW_MILEAGE_COST * ceil_weeks
+
+            elif num_miles > (WEEKLY_HIGH_MILEAGE_ALLOWANCE * weeks):
+                cost += WEEKLY_HIGH_MILEAGE_COST * ceil_weeks
+                cost += (num_miles - \
+                (WEEKLY_HIGH_MILEAGE_ALLOWANCE * ceil_weeks)) * MILEAGE_COST
+
+
+        print("\n\nCustomer summary:")
+        print("\tclassification code:", code)
+        print("\trental period (days):", days)
+        print("\todometer reading at start:", int(odometer_start))
+        print("\todometer reading at end:  ", int(odometer_end))
+        print("\tnumber of miles driven: ", round(num_miles, 1))
+        print("\tamount due: $", float(round(cost, 2)))
+    else:
+        print("\n\t*** Invalid customer code. Try again. ***")
+        continue
+
+    done = input("\nWould you like to continue (A/B)? ")
+
+print("\nThank you for your loyalty.")
