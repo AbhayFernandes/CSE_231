1015c76f8a53c6c327f93ec6ebdb4ae7b1246a2f
Abhay Fernandes <abhay.fernandes@protonmail.com>
2023-02-09 16:57:13 -0500

    added comments

--- project04/main.py
+++ project04/main.py
@@ -21,6 +21,7 @@ BANNER = """
 def numtobase(N, B):
     """convert a number N to a different base system B"""
     string = ""
+    # if N is 0, return an empty string
     if N == 0:
         return ""
     while N > 0:
@@ -39,8 +40,11 @@ def basetonum(S, B):
         return 0
     S = int(S)
     num = 0
+    # loop through each digit in S
     for i in range(1, len(str(S)) + 1):
         digit = S % 10
+        # add the digit multiplied by the base raised to the power of the
+        # digit's position to the total
         num += digit * (B ** (i - 1))
         S = S // 10
     return num
@@ -68,10 +72,13 @@ def encode_image(image, text, N):
     text_to_binary = convert_to_binary(text)
     for i in range(0, len(image), N):
         try:
+            # replace the last bit of the pixel with the first bit of the text
             new_bit = image[i : i + N]
             new_bit = new_bit[: N - 1] + text_to_binary[0]
             text_to_binary = text_to_binary[1:]
+            # add the new pixel to the new image
             new_image += new_bit
+        # if there is no more text to encode, add the rest of the image
         except IndexError:
             new_image += image[i : i + N]
     return new_image
@@ -82,15 +89,20 @@ def decode_image(stego, N):
     LSB and N representing how many bits per pixel"""
     binary_text = ""
     output_string = ""
+    # loop through every Nth pixel and add the last bit to the binary text
     for i in range(N - 1, len(stego), N):
         binary_text += stego[i]
+    # remove any extra bits at the end
     binary_text = binary_text[: len(binary_text) - len(binary_text) % 8]
+    # convert the binary text to a string
     for i in range(0, len(binary_text), 8):
         output_string += chr(basetonum(binary_text[i : i + 8], 2))
     return output_string
 
 
 def input_positive_int(prompt):
+    """Prompt the user for a positive integer and return it.
+    Using error checking."""
     while True:
         num = input(prompt)
         try:
@@ -113,6 +125,8 @@ def input_positive_int(prompt):
 
 
 def input_valid_base(prompt):
+    """Prompt the user for a valid base and return it.
+    Using error checking."""
     while True:
         base = input(prompt)
         try:
@@ -143,19 +157,23 @@ def input_valid_base(prompt):
 def main():
     print(BANNER)
     print(MENU)
+    # loop until the user enters X
     while True:
         option = input("\n\tEnter option: ").upper()
         if option == "M":
             print(MENU)
         elif option == "A":
+            # get the number and base from the user, ouput the result
             num = input_positive_int("\n\tEnter N: ")
             base = input_valid_base("\n\tEnter Base: ")
             print(f"\n\t {num} in base {base}: {numtobase(num, base)}")
         elif option == "B":
+            # get the string and base from the user, ouput the result
             S = input("\n\tEnter string number S: ")
             B = input_valid_base("\n\tEnter Base: ")
             print(f"\n\t {S} in base {B}: {basetonum(S, B)}")
         elif option == "C":
+            # get the number, base and new base from the user, ouput the result
             B1 = input_valid_base("\n\tEnter base B1: ")
             B2 = input_valid_base("\n\tEnter base B2: ")
             S = input("\n\tEnter string number: ")
@@ -164,6 +182,7 @@ def main():
                 f"{basetobase(B1, B2, S)} in base {B2}..."
             )
         elif option == "E":
+            # get the image, number of bits per pixel and text from the user
             S = input("\n\tEnter a binary string of an image: ")
             bits = input_positive_int(
                 "\n\tEnter number of bits used for pixels: "
@@ -179,10 +198,12 @@ def main():
                 print(f"\n\t Original image: {S}")
                 print(f"\n\t Encoded image: {encode_image(S, text, bits)}")
         elif option == "D":
+            # get the image and number of bits per pixel from the user
             stego = input("\n\tEnter an encoded string of an image: ")
             bits = input_positive_int(
                 "\n\tEnter number of bits used for pixels: "
             )
+            # decode the image and output the result
             print(f"\n\t Original text: {decode_image(stego, bits)}")
         elif option == "X":
             break
