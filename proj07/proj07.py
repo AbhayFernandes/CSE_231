d31e194d9b8ce1e5bf4d62ae9d4130dc28042ebf
Abhay Fernandes <abhay.fernandes@protonmail.com>
2023-03-21 23:00:13 -0400

    added final display_statistics function

--- proj07/proj07.py
+++ proj07/proj07.py
@@ -24,23 +24,23 @@ MENU = '''
 
 def open_files() -> Union[List[str], List[TextIO]]:
     ''' Docstring'''
-    cities = []
-    cities_fp = []
     while True:
+        cities = []
+        cities_fp = []
         str_list = input("Enter cities names: ")
         str_list = str_list.strip()
         str_list = str_list.split(",")
         for city in str_list:
             try:
                 city = city.strip()
-                cities.append(city)
                 city_fp = open(city + ".csv", "rt")
+                cities.append(city)
                 cities_fp.append(city_fp)
             except FileNotFoundError:
                 print(f"Error: File {city}.csv is not found")
                 break
-        break
-    return cities, cities_fp
+        if len(cities) == len(str_list):
+            return cities, cities_fp
 
 
 def null_float(value: str) -> float:
@@ -100,7 +100,7 @@ def get_min(col: int, data: List[List[Tuple[str, float, float, float, float, flo
     return output   # remove this line
 
 
-def get_max(col, data, cities): 
+def get_max(col: int, data: List[List[Tuple[str, float, float, float, float, float, float]]], cities: List[str]) -> Tuple[str, float]: 
     ''' Docstring'''
     max_values = []
     for city in data:
@@ -153,11 +153,11 @@ def mode(lst):
             streak_num = lst[i]
             streak_count = 1
         counts.append((streak_num, streak_count))
-        counts = remove_duplicates(counts)
+        counts = list(set((counts)))
     return multi_max(counts), max(counts, key=itemgetter(1))[1]
 
 
-def get_average(col, data, cities): 
+def get_average(col: int, data: List[List[Tuple[str, float, float, float, float, float, float]]], cities: List[str]) -> Tuple[str, float]: 
     ''' Docstring'''
     avg_values = []
     for city in data:
@@ -169,10 +169,10 @@ def get_average(col, data, cities):
     output = []
     for city in cities:
         output.append((city, round(avg_values[cities.index(city)], 2)))
-    return output   # remove this line
+    return output
 
 
-def get_modes(col, data, cities):
+def get_modes(col: int, data: List[List[Tuple[str, float, float, float, float, float, float]]], cities: List[str]) -> Tuple[str, float]:
     ''' Docstring'''
     mode_values = []
     for city in data:
@@ -187,36 +187,45 @@ def get_modes(col, data, cities):
             output.append((city, [], mode_values[cities.index(city)][1]))
         else:
             output.append((city, mode_values[cities.index(city)][0], mode_values[cities.index(city)][1]))
-    return output   # remove this line
+    return output
 
 
 def high_low_averages(data, cities, categories):
     ''' Docstring'''
-    pass   # remove this line
+    output = []
+    for category in categories:
+        try:
+            col = COLUMNS.index(category)
+            averages = get_average(col, data, cities)
+            averages = sorted(averages, key=itemgetter(0))
+            averages = sorted(averages, key=itemgetter(1))
+            output.append([(averages[0][0], averages[0][1]), (averages[-1][0], averages[-1][1])])
+        except ValueError:
+            output.append(None)
+    return output 
 
 
-def display_statistics(col,data, cities):
+def display_statistics(col, data, cities):
     ''' Docstring'''
+    averages = get_average(col, data, cities)
+    modes = get_modes(col, data, cities)
+    mins = get_min(col, data, cities)
+    maxs = get_max(col, data, cities)
+    modes_str = ""
+    for i in modes:
+        modes_str = modes_str.join(f"{i[1][0]},")
+    modes_str = modes_str[:-1]
+    for i in range(len(cities)):
+        print(f"{cities[i]}:")
+        print(f"Min: {mins[i][1]:.2f} Max: {maxs[i][1]:.2f} Avg: {averages[i][1]:.2f}")
+        print(f"Most common repeated values ({modes[0][2]} occurrences): {modes_str}")
     pass   # remove this line
 
 
 def main():
-    ''' Docstring'''
-    data = [[('1/1/1948', None, 26.0, 22.5, 0.84, 3.1, 3.0), ('1/2/1948', None, 27.0,
-            22.0, 0.09, 0.9, 5.0), ('11/18/2022', 28.0, 31.0, 28.0, 0.12, 2.6, 5.0),
-            ('12/25/2022', 17.0, 18.0, 13.0, 0.01, 0.4, 9.0)], [('1/1/1948', None, 26.0,
-            22.0, 0.84, 3.1, 3.0), ('1/2/1948', None, 27.0, 23.2, 0.09, 0.9, 5.0),
-            ('12/25/2022', 17.0, 18.0, 13.0, 0.01, 0.4, 9.0), ('12/25/2020', 17.0, 18.0,
-            22.4, 0.01, 0.4, 9.0),('11/18/2022', 28.0, 26.4, 13.17, 0.12, 2.6, 5.0)],
-            [('11/17/2000', 31.0, 34.0, 28.0, 0.0, 0.1, None), ('11/18/2000', 29.0, 35.0, -
-            26.0, 0.0, None, None), ('11/19/2000', 33.0, 43.0, 20.0, 0.02, None, None),
-            ('11/20/2000', 25.0, 29.0, -26.0, 0.0, 0.3, 0.0), ('12/25/2020', 17.0, 18.0,
-            22.4, 0.01, 0.4, 9.0),('11/18/2022', 28.0, 22.0, -26.47, 0.12, 2.6,
-            5.0),('12/25/2022', 17.0, 18.0, -26.32, 0.01, 0.4, 9.0),('11/25/2022', 17.0,
-            18.0, -25.97, 0.01, 0.4, 9.0)]]
-    cities = ['FL', 'CA', 'MI']
-    col = 3
-    print(get_modes(col, data, cities))
+    cities, fps = open_files()
+    master_list = read_files(fps)
+    display_statistics(3, master_list, cities)
     pass
 
 if __name__ == "__main__":
